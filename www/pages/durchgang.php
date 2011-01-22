0f6f050ed6b08801ea4fa5195e44faa749c7ab33
Friedrich Schöller <code@schoeller.se>
2011-01-23 00:41:35 +0100

    js path fixed

--- www/pages/durchgang.php
+++ www/pages/durchgang.php
@@ -8,7 +8,7 @@ class InputPage extends Page
     public function draw()
     {
         $this->drawHeader("Input",
-            array("mootools.js", "game.js", "input.js", "inputstates.js", "graphics.js"),
+            array("js/mootools.js", "js/game.js", "js/input.js", "js/inputstates.js", "js/graphics.js"),
             array("input.css"));
 
         print('<canvas id="game" width="640" height="480"></canvas>');

6eb668957bf4f6b8c1f8a3db0499cda5b2b9db7c
Friedrich Schöller <code@schoeller.se>
2011-01-22 22:58:37 +0100

    fixed url encoding

--- www/html/header.php
+++ www/html/header.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <title><?=htmlspecialchars($title)?></title>
 <?php foreach ($javaScripts as $script) { ?>
-    <script type="text/javascript" src="<?=rawurlencode($script)?>"></script>
+    <script type="text/javascript" src="<?=htmlspecialchars($script)?>"></script>
 <?php } ?>
 <?php foreach ($styleSheets as $sheet) { ?>
-    <link rel="stylesheet" type="text/css" href="<?=rawurlencode($sheet)?>">
+    <link rel="stylesheet" type="text/css" href="<?=htmlspecialchars($sheet)?>">
 <?php } ?>
   </head>
   <body>

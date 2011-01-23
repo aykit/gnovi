a3dd40cd9b222e70c7ffd1dfc7675979dadd3c31
maltefiala <maltefiala@gmx.at>
2011-01-23 02:33:26 +0100

    nice code for nice coders

--- www/html/header.php
+++ www/html/header.php
@@ -1,27 +1,30 @@
 <!DOCTYPE HTML>
 <html>
   <head>
-    <meta charset="UTF-8">
-    <title><?=htmlspecialchars($title)?></title>
+  <meta charset="UTF-8">
+  <title><?=htmlspecialchars($title)?></title>
 <?php foreach ($javaScripts as $script) { ?>
-    <script type="text/javascript" src="<?=htmlspecialchars($script)?>"></script>
+  <script type="text/javascript" src="<?=htmlspecialchars($script)?>"></script>
 <?php } ?>
 <?php foreach ($styleSheets as $sheet) { ?>
-    <link rel="stylesheet" type="text/css" href="<?=htmlspecialchars($sheet)?>">
+  <link rel="stylesheet" type="text/css" href="<?=htmlspecialchars($sheet)?>">
 <?php } ?>
   </head>
   <body>
-<nav>
-	<ul id="ulfront"><li>
-		<ul id="nav">
-		<li><img src="http://gnovi.org/test/images/gnovi_klein.png" alt="gnovi_klein" width="35" height="38" /></li>
-		<li>Profil</li>
-		<li>Anlauf</li>
-		<li>Ich</li>
-		<li>Wir</li>
-		<li>Hilfe</li>
-		<li>&uuml;ber gnovi</li>
-		</ul>
-	</li></ul>
-</nav>
-<br /><div id="navbar"></div>
+    <nav>
+      <ul id="ulfront">
+        <li>
+          <ul id="nav">
+            <li><img src="http://gnovi.org/test/images/gnovi_klein.png" alt="gnovi_klein" width="35" height="38"></li>
+            <li>Profil</li>
+            <li>Anlauf</li>
+            <li>Ich</li>
+            <li>Wir</li>
+            <li>Hilfe</li>
+            <li>&uuml;ber gnovi</li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <br>
+    <div id="navbar"></div>

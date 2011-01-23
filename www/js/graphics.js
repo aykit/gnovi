1fd4372e8f78f70f6f859d2a10e415855ac1c4c6
maltefiala <maltefiala@gmx.at>
2011-01-23 19:44:29 +0100

    An Open Letter to Tokyo _コード gives me more than pleasure

--- www/js/graphics.js
+++ www/js/graphics.js
@@ -4,270 +4,292 @@
  */
 
 var Graphics = new Class({
-    setContext: function(context) { this.context = context; },
+  setContext: function(context) { this.context = context; },
 
-    _fillCenteredText: function(text, posX, posY)
+  _fillCenteredText: function(text, posX, posY)
+  {
+    this.context.fillText(text, posX - this.context.measureText(text).width / 2, posY)
+  },
+
+  _strokeCenteredText: function(text, posX, posY)
+  {
+    this.context.strokeText(text, posX - this.context.measureText(text).width / 2, posY)
+  },
+
+  _fillCircle: function(x, y, r)
+  {
+    this.context.beginPath();
+    this.context.arc(x, y, r, 0, 2 * Math.PI, false);
+    this.context.fill();
+  },
+  
+  _コード: function(x,y)
+  {
+    this.context.beginPath();
+    this.context.strokeStyle = "#7F7F7F";
+    this.context.moveTo(x, y);
+    this.context.lineTo(x+100, y);
+    this.context.quadraticCurveTo(x+112, y, x+112, y+12);
+    this.context.lineTo(x+112, y+32);
+    this.context.quadraticCurveTo(x+112, y+44, x+100, y+44);
+    this.context.lineTo(x, y+44);
+    this.context.quadraticCurveTo(x-12, y+44, x-12, y+32);
+    this.context.lineTo(x-12, y+12);
+    this.context.quadraticCurveTo(x-12, y, x, y);
+    this.context.stroke();
+    this.context.fillStyle = "#231F20";
+    this.context.fill();
+    this.context.stroke();
+
+  },
+
+  _drawGnoviIcon: function(posX, posY, size, glow, alpha)
+  {
+    var gradient = this.context.createRadialGradient(posX, posY, 0, posX, posY, size / 2);
+    gradient.addColorStop(0.3, "rgba(255, 255, 255, " + alpha + ")");
+    gradient.addColorStop(0.5, "rgba(0, 0, 0, " + alpha + ")");
+    gradient.addColorStop(0.6, "rgba(0, 255, 0, " + alpha + ")");
+
+    if (glow)
+      gradient.addColorStop(1, "rgba(255, 0, 0, " + alpha + ")");
+    else
+      gradient.addColorStop(1, "rgba(0, 0, 255, " + alpha + ")");
+
+    this.context.fillStyle = gradient;
+    this._fillCircle(posX, posY, size / 2);
+    //this.context.putImageData(this.getImage("google"), posX, posY);
+  },
+
+  _fillTextRect: function(wordList, x, y, width, spacing, lineHeight)
+  {
+  var wordOffsetX = 0;
+  var wordOffsetY = 0;
+
+  for (var i = 0; i < wordList.length; i++)
+  {
+    var textWidth = this.context.measureText(wordList[i]).width;
+
+    if (wordOffsetX != 0 && wordOffsetX + textWidth > width)
     {
-        this.context.fillText(text, posX - this.context.measureText(text).width / 2, posY)
-    },
+    wordOffsetX = 0;
+    wordOffsetY += lineHeight;
+    }
 
-    _strokeCenteredText: function(text, posX, posY)
-    {
-        this.context.strokeText(text, posX - this.context.measureText(text).width / 2, posY)
-    },
+    this.context.fillText(wordList[i], x + wordOffsetX, y + wordOffsetY);
 
-    _fillCircle: function(x, y, r)
-    {
-        this.context.beginPath();
-        this.context.arc(x, y, r, 0, 2 * Math.PI, false);
-        this.context.fill();
-    },
+    wordOffsetX += textWidth + spacing;
+  }
+  },
 
-    _drawGnoviIcon: function(posX, posY, size, glow, alpha)
-    {
-        var gradient = this.context.createRadialGradient(posX, posY, 0, posX, posY, size / 2);
-        gradient.addColorStop(0.3, "rgba(255, 255, 255, " + alpha + ")");
-        gradient.addColorStop(0.5, "rgba(0, 0, 0, " + alpha + ")");
-        gradient.addColorStop(0.6, "rgba(0, 255, 0, " + alpha + ")");
+  _clearCanvas: function()
+  {
+  this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+  },
 
-        if (glow)
-            gradient.addColorStop(1, "rgba(255, 0, 0, " + alpha + ")");
-        else
-            gradient.addColorStop(1, "rgba(0, 0, 255, " + alpha + ")");
+  drawDebugInfo: function(fps, drawCount)
+  {
+  this.context.translate(this.context.canvas.width - 40, this.context.canvas.height - 50);
 
-        this.context.fillStyle = gradient;
-        this._fillCircle(posX, posY, size / 2);
-        //this.context.putImageData(this.getImage("google"), posX, posY);
-    },
+  this.context.shadowColor = "black";
+  this.context.shadowBlur = 2;
+  this.context.shadowOffsetX = 1;
+  this.context.shadowOffsetY = 1;
+  this.context.strokeStyle = "black";
+  this.context.fillStyle = "black";
 
-    _fillTextRect: function(wordList, x, y, width, spacing, lineHeight)
-    {
-        var wordOffsetX = 0;
-        var wordOffsetY = 0;
+  this.context.font = "8px";
+  var txt = Math.round(fps) + " fps";
+  this._fillCenteredText(txt, 0, 30);
 
-        for (var i = 0; i < wordList.length; i++)
-        {
-            var textWidth = this.context.measureText(wordList[i]).width;
+  this.context.rotate(drawCount * 2 * Math.PI / 64);
+  this.context.strokeRect(-10, -10, 20, 20);
+  },
 
-            if (wordOffsetX != 0 && wordOffsetX + textWidth > width)
-            {
-                wordOffsetX = 0;
-                wordOffsetY += lineHeight;
-            }
+  drawLoadingIndicator: function(loadTime)
+  {
+     loadTime -= 0.2;
+  if (loadTime < 0)
+    return;
 
-            this.context.fillText(wordList[i], x + wordOffsetX, y + wordOffsetY);
+  alpha = loadTime;
+  if (alpha > 1)
+    alpha = 1;
 
-            wordOffsetX += textWidth + spacing;
-        }
-    },
+  scale1 = loadTime * 10;
+  if (scale1 > 5)
+    scale1 = 5;
 
-    _clearCanvas: function()
-    {
-        this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
-    },
+  scale2 = 10 - loadTime * 10;
+  if (scale2 < 3)
+    scale2 = 3;
 
-    drawDebugInfo: function(fps, drawCount)
-    {
-        this.context.translate(this.context.canvas.width - 40, this.context.canvas.height - 50);
+  this.context.strokeStyle = "rgba(0, 0, 0," + alpha + ")";
 
-        this.context.shadowColor = "black";
-        this.context.shadowBlur = 2;
-        this.context.shadowOffsetX = 1;
-        this.context.shadowOffsetY = 1;
-        this.context.strokeStyle = "black";
-        this.context.fillStyle = "black";
+  this.context.save();
+  this.context.translate(this.context.canvas.width / 2, this.context.canvas.height / 2);
+  this.context.rotate(loadTime * 2 * Math.PI / 4);
+  this.context.scale(scale1, scale1);
+  this.context.strokeRect(-10, -10, 20, 20);
+  this.context.restore();
 
-        this.context.font = "8px";
-        var txt = Math.round(fps) + " fps";
-        this._fillCenteredText(txt, 0, 30);
+  this.context.save();
+  this.context.translate(this.context.canvas.width / 2, this.context.canvas.height / 2);
+  this.context.rotate(- loadTime * 2 * Math.PI / 4);
+  this.context.scale(scale2, scale2);
+  this.context.strokeRect(-10, -10, 20, 20);
+  this.context.restore();
 
-        this.context.rotate(drawCount * 2 * Math.PI / 64);
-        this.context.strokeRect(-10, -10, 20, 20);
-    },
+  this.context.textBaseline = "middle";
+  this.context.font = "bold 12px Verdana";
 
-    drawLoadingIndicator: function(loadTime)
-    {
-       loadTime -= 0.2;
-        if (loadTime < 0)
-            return;
+  this.context.strokeStyle = "rgba(255, 255, 255," + alpha + ")";
+  this.context.lineWidth = 5;
+  this._strokeCenteredText("loading . . .", 0, 200)
 
-        alpha = loadTime;
-        if (alpha > 1)
-            alpha = 1;
-
-        scale1 = loadTime * 10;
-        if (scale1 > 5)
-            scale1 = 5;
-
-        scale2 = 10 - loadTime * 10;
-        if (scale2 < 3)
-            scale2 = 3;
-
-        this.context.strokeStyle = "rgba(0, 0, 0," + alpha + ")";
-
-        this.context.save();
-        this.context.translate(this.context.canvas.width / 2, this.context.canvas.height / 2);
-        this.context.rotate(loadTime * 2 * Math.PI / 4);
-        this.context.scale(scale1, scale1);
-        this.context.strokeRect(-10, -10, 20, 20);
-        this.context.restore();
-
-        this.context.save();
-        this.context.translate(this.context.canvas.width / 2, this.context.canvas.height / 2);
-        this.context.rotate(- loadTime * 2 * Math.PI / 4);
-        this.context.scale(scale2, scale2);
-        this.context.strokeRect(-10, -10, 20, 20);
-        this.context.restore();
-
-        this.context.textBaseline = "middle";
-        this.context.font = "bold 12px Verdana";
-
-        this.context.strokeStyle = "rgba(255, 255, 255," + alpha + ")";
-        this.context.lineWidth = 5;
-        this._strokeCenteredText("loading . . .", 0, 200)
-
-        this.context.fillStyle = "rgba(0, 0, 0," + alpha + ")";
-        this._fillCenteredText("loading . . .", this.context.canvas.width / 2, this.context.canvas.height - 30)
-    },
+  this.context.fillStyle = "rgba(0, 0, 0," + alpha + ")";
+  this._fillCenteredText("loading . . .", this.context.canvas.width / 2, this.context.canvas.height - 30)
+  },
 });
 
 var InputGraphics = new Class({
-    Extends: Graphics,
+  Extends: Graphics,
 
-    drawStartScreen: function()
-    {
-        this._clearCanvas();
+  drawStartScreen: function()
+  {
+  this._clearCanvas();
 
-        this.context.fillStyle = "black";
+  this.context.fillStyle = "black";
 
-        this.context.font = "bold 20px Courier New";
-        this.context.fillText("click to start", 100, 20);
-    },
+  this.context.font = "bold 20px Courier New";
+  this.context.fillText("click to start", 100, 20);
+  },
 
-    drawWordsFinishedScreen: function(wordList, inputTime, alpha, drawContinueNotice)
-    {
-        this._clearCanvas();
+  drawWordsFinishedScreen: function(wordList, inputTime, alpha, drawContinueNotice)
+  {
+  this._clearCanvas();
 
-        this.context.fillStyle = "rgba(0, 0, 0, " + alpha + ")";
+  this.context.fillStyle = "rgba(0, 0, 0, " + alpha + ")";
 
-        this.context.font = "bold 20px Courier New";
+  this.context.font = "bold 20px Courier New";
 
-        this.context.fillText(wordList.length + " words entered in " + inputTime + " seconds", 20, 20);
+  this.context.fillText(wordList.length + " words entered in " + inputTime + " seconds", 20, 20);
 
-        this._fillTextRect(wordList, 40, 50, 300, 10, 25);
+  this._fillTextRect(wordList, 40, 50, 300, 10, 25);
 
-        if (!drawContinueNotice)
-            return;
+  if (!drawContinueNotice)
+    return;
 
-        this.context.font = "bold 14px Courier New";
-        this.context.fillText("press Enter to continue", 20, 300);
-    },
+  this.context.font = "bold 14px Courier New";
+  this.context.fillText("press Enter to continue", 20, 300);
+  },
 
-    drawLocationWordsFinishedScreen: function(wordList, inputTime, alpha, drawContinueNotice)
-    {
-        this.drawWordsFinishedScreen(wordList, inputTime, alpha, drawContinueNotice);
-    },
+  drawLocationWordsFinishedScreen: function(wordList, inputTime, alpha, drawContinueNotice)
+  {
+  this.drawWordsFinishedScreen(wordList, inputTime, alpha, drawContinueNotice);
+  },
 
-    drawInputLocationScreen: function(inputText)
-    {
-        this._clearCanvas();
+  drawInputLocationScreen: function(inputText)
+  {
+  this._clearCanvas();
 
-        this.context.fillStyle = "black";
-        this.context.font = "bold 20px Courier New";
+  this.context.fillStyle = "black";
+  this.context.font = "bold 20px Courier New";
 
-        this.context.fillText("Enter your location:", 20, 20);
+  this.context.fillText("Enter your location:", 20, 20);
 
-        this.context.font = "bold 15px Courier New";
-        this.context.fillText(inputText, 30, 50);
-    },
+  this.context.font = "bold 15px Courier New";
+  this.context.fillText(inputText, 30, 50);
+  },
 
-    drawWordCollectingScreen: function(headWord, timeLeft, totalTime, currentInputText, inputList, inputListAnimation)
-    {
-        this._clearCanvas();
+  drawWordCollectingScreen: function(headWord, timeLeft, totalTime, currentInputText, inputList, inputListAnimation)
+  {
+  this._clearCanvas();
 
-        this.context.fillStyle = "black";
+  this.context.fillStyle = "white";
+  this.context.font = "25px Hero";
+  this.context.textAlign = "center";
+  this.context.fillText(headWord, 320, 135);
+  this._コード(100, 150);
 
-        this.context.font = "bold 25px Hero";
-        this.context.fillText(headWord, 20, 20);
+  this.context.font = "bold 20px Courier New";
+  this.context.fillText(Math.ceil(timeLeft), 400, 20);
 
-        this.context.font = "bold 20px Courier New";
-        this.context.fillText(Math.ceil(timeLeft), 400, 20);
+  /* TIME */
+  this.context.fillStyle = "#A7CF4A";
+  this.context.strokeStyle = "#7F7F7F";
+  this.context.lineCap = "round";
+  this.context.lineJoin = "round";
+  this.context.lineWidth = "1.5";
+  this.context.fillRect(0, 76, timeLeft / totalTime * 640, 24);
+  this.context.strokeRect(0, 75.5, 640, 25);  
 
-        /* TIME */
-        this.context.fillStyle = "#A7CF4A";
-        this.context.strokeStyle = "#7F7F7F";
-        this.context.lineCap = "round";
-        this.context.lineJoin = "round";
-        this.context.fillRect(0, 76, timeLeft / totalTime * 640, 24);
-        this.context.strokeRect(0, 75, 640, 25);
-        
 
-        this.context.font = "bold 15px Courier New";
-        this.context.fillText(currentInputText, 20, 300);
+  this.context.font = "bold 15px Courier New";
+  this.context.fillText(currentInputText, 20, 300);
 
-        for (var i = 0; i < inputList.length; i++)
-        {
-            var a = inputListAnimation[i];
-            var b = 1 - a;
-            this.context.fillText(inputList[i], 20, (i * 20 + 50) * a + 300 * b );
-        }
-    },
+  for (var i = 0; i < inputList.length; i++)
+  {
+    var a = inputListAnimation[i];
+    var b = 1 - a;
+    this.context.fillText(inputList[i], 20, (i * 20 + 50) * a + 300 * b );
+  }
+  },
 
-    drawWordRatingScreen: function(word)
-    {
-        this.context.font = "bold 20px Courier New";
-        this.context.fillText(word, 10, 30);
+  drawWordRatingScreen: function(word)
+  {
+  this.context.font = "bold 20px Courier New";
+  this.context.fillText(word, 10, 30);
 
-        this.context.font = "bold 30px Courier New";
-        this.context.fillText("+ -", 10, 60);
-    },
+  this.context.font = "bold 30px Courier New";
+  this.context.fillText("+ -", 10, 60);
+  },
 });
 
 var GraphGraphics = new Class({
-    Extends: Graphics,
+  Extends: Graphics,
 
-    drawBackground: function()
-    {
-        this._clearCanvas();
-    },
+  drawBackground: function()
+  {
+  this._clearCanvas();
+  },
 
-    drawNode: function(node, posX, posY, isRoot, mouseOver, alpha)
-    {
-        this.context.textBaseline = "middle";
+  drawNode: function(node, posX, posY, isRoot, mouseOver, alpha)
+  {
+  this.context.textBaseline = "middle";
 
-        if (isRoot)
-        {
-            this.context.font = "bold 14px Verdana";
-            this._drawGnoviIcon(posX, posY, 50, true, alpha);
-            this.context.fillStyle = "rgba(255, 0, 0, " + alpha + ")";
-            this._fillCenteredText(node.label, posX, posY + 40);
-        }
-        else
-        {
-            this.context.font = "bold 10px Verdana";
-            this._drawGnoviIcon(posX, posY, 30, mouseOver, alpha);
-            this.context.fillStyle = "rgba(0, 0, 0, " + alpha + ")";
-            this._fillCenteredText(node.label, posX, posY + 25);
-        }
-    },
+  if (isRoot)
+  {
+    this.context.font = "bold 14px Verdana";
+    this._drawGnoviIcon(posX, posY, 50, true, alpha);
+    this.context.fillStyle = "rgba(255, 0, 0, " + alpha + ")";
+    this._fillCenteredText(node.label, posX, posY + 40);
+  }
+  else
+  {
+    this.context.font = "bold 10px Verdana";
+    this._drawGnoviIcon(posX, posY, 30, mouseOver, alpha);
+    this.context.fillStyle = "rgba(0, 0, 0, " + alpha + ")";
+    this._fillCenteredText(node.label, posX, posY + 25);
+  }
+  },
 
-    drawConnection: function(x1, y1, x2, y2, alpha)
-    {
-        this.context.beginPath();
-        this.context.moveTo(x1, y1);
-        this.context.lineTo(x2, y2);
+  drawConnection: function(x1, y1, x2, y2, alpha)
+  {
+  this.context.beginPath();
+  this.context.moveTo(x1, y1);
+  this.context.lineTo(x2, y2);
 
-        this.context.strokeStyle = "rgba(0, 0, 0, " + alpha + ")";
-        this.context.lineWidth = 1.5;
-        this.context.stroke();
-    },
+  this.context.strokeStyle = "rgba(0, 0, 0, " + alpha + ")";
+  this.context.lineWidth = 1.5;
+  this.context.stroke();
+  },
 
-    getGraphCenter: function()
-    {
-        return {x: 320, y: 240};
-    },
+  getGraphCenter: function()
+  {
+  return {x: 320, y: 240};
+  },
 
-    getNodeStartDistance: function() { return 350; },
+  getNodeStartDistance: function() { return 350; },
 
-    getInterpolationTime: function() { return 1.8; },
+  getInterpolationTime: function() { return 1.8; },
 });

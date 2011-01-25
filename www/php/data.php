70683ebf1e0f24be4a408779262711506fe577bc
Friedrich Schöller <code@schoeller.se>
2011-01-25 01:37:22 +0100

    made data.php object oriented

--- www/php/data.php
+++ www/php/data.php
@@ -3,91 +3,97 @@
 require_once "database.php";
 require_once "utilities.php";
 
-switch (@$_GET["cmd"])
+class DataExchanger
 {
-case "getword":
-    getWord();
-    break;
-case "getgraph":
-    getGraph(Utilities::urlDecode((string)@$_GET["word"]));
-    break;
+    public function processRequest($request)
+    {
+        switch (@$request["cmd"])
+        {
+        case "getword":
+            $this->returnRandomWord();
+            break;
+        case "getgraph":
+            $this->returnGraphData(Utilities::urlDecode((string)@$request["word"]));
+            break;
+        default:
+            $this->setResponseError("Unknown command");
+        }
+
+        print(json_encode($this->response));
+    }
+
+    protected function returnRandomWord()
+    {
+        if (!$this->connectDb())
+            return;
+
+        $this->setResponseData("Haus");
+    }
+
+    protected function returnGraphData($word)
+    {
+        if (!$this->connectDb())
+            return;
+
+        $data1 = array(
+            "root" => array("id" => 1, "label" => "Haus"),
+            "nodes" => array(
+                array("id" => 32, "label" => "du"),
+                array("id" => 3, "label" => "ich"),
+                array("id" => 4, "label" => "bla"),
+                array("id" => 15, "label" => "blub"),
+                array("id" => 6, "label" => "genau"),
+            )
+        );
+
+        $data2 = array(
+            "root" => array("id" => 15, "label" => "blub"),
+            "nodes" => array(
+                array("id" => 3, "label" => "ich"),
+                array("id" => 1, "label" => "Haus"),
+                array("id" => 11, "label" => "warum"),
+                array("id" => 32, "label" => "du"),
+                array("id" => 7, "label" => "nix"),
+                array("id" => 8, "label" => "jap"),
+            )
+        );
+
+        $data = $word == "Haus" ? $data1 : $data2;
+
+        $this->setResponseData($data);
+    }
+
+    protected function connectDb()
+    {
+        if ($this->db)
+            return true;
+
+        $this->db = new Database();
+        if (!$this->db->connect())
+        {
+            $this->setResponseError("Database: " . $this->db->connectError);
+            $this->db = null;
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function setResponseError($error)
+    {
+        $this->response = array("status" => "error", "error" => (string)$error);
+    }
+
+    protected function setResponseData($data)
+    {
+        $this->response = array("status" => "success", "data" => $data);
+    }
+
+    protected $response = null;
+    protected $db = null;
 }
 
-function getWord()
-{
-    $db = getDb();
-    exitWithData("Haus");
-}
-
-function getGraph($word)
-{
-    $db = getDb();
-
-    $word = json_encode($word);
-
-    $sampleA = '{
-        "root": {"id": 1, "label": ' . $word . '},
-        "nodes":
-        [
-            {"id": 32, "label": "du"},
-            {"id": 3, "label": "ich"},
-            {"id": 4, "label": "bla"},
-            {"id": 15, "label": "blub"},
-            {"id": 6, "label": "genau"},
-        ],
-        "relations":
-        [
-            {"id2": 2, "id2": 3, "strength": 9},
-        ],
-    }';
-
-    $sampleB = '{
-        "root": {"id": 4, "label": "bla"},
-        "nodes":
-        [
-            {"id": 3, "label": "ich"},
-            {"id": 1, "label": "root"},
-            {"id": 11, "label": "warum"},
-            {"id": 32, "label": "du"},
-            {"id": 7, "label": "nix"},
-            {"id": 8, "label": "jap"},
-        ],
-        "relations":
-        [
-            {"id2": 2, "id2": 3, "strength": 9},
-        ],
-    }';
-
-    print('{"status":"success", "data":');
-    if ($word == "bla")
-        print($sampleB);
-    else
-        print($sampleA);
-    print('}');
-}
-
-function getDb()
-{
-    $db = new Database();
-    if (!$db->connect())
-        exitWithError($db->connectError);
-    return $db;
-}
-
-function exitWithError($error)
-{
-    exit(json_encode(array(
-        "status" => "error",
-        "error" => (string)$error,
-    )));
-}
-
-function exitWithData($data)
-{
-    exit(json_encode(array(
-        "status" => "success",
-        "data" => $data,
-    )));
-}
+$dataExchanger = new DataExchanger();
+$dataExchanger->processRequest($_GET);
 
 ?>

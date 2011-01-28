ff26f609fedc13df8bcf4b8ecc000f00628b5a2a
Friedrich Schöller <code@schoeller.se>
2011-01-28 18:43:37 +0100

    finished database input

--- www/php/data.php
+++ www/php/data.php
@@ -45,7 +45,7 @@ class DataExchanger
         if (!$this->connectDb())
             return;
 
-        $this->setResponseData("Haus");
+        $this->setResponseData("Fernseher");
         //$this->setResponseData("/!%66; ?_:/.@&=+$,ößюфド\\%%\\\\1");
     }
 
@@ -54,10 +54,18 @@ class DataExchanger
         if (!$this->connectDb())
             return null;
 
-        // TODO: lookup the letter case in the word database
-        $word = ucfirst($word);
-
         $escWord = $this->db->escape_string($word);
+
+        $result = $this->db->query("SELECT `Word` FROM `Wordcheck` WHERE `Word` = '$escWord'");
+        if (!$this->checkForDbError())
+            return null;
+
+        $row = $result->fetch_assoc();
+        if ($row)
+        {
+            $word = $row["Word"];
+            $escWord = $this->db->escape_string($word);
+        }
 
         // Insert the word or update the entry to reflect letter case changes
         $this->db->query("INSERT INTO `Words` (`Word`) VALUES ('$escWord') " .
@@ -98,6 +106,9 @@ class DataExchanger
         $wordMap = array();
         foreach ($words as $word)
         {
+            if ($word == "." || $word == "..")
+                continue;
+
             $wordInfo = $this->findWord($word);
             if (!$wordInfo)
                 return;
@@ -121,45 +132,164 @@ class DataExchanger
             return;
         }
 
-        if (!$this->connectDb())
-            return;
-
-        $escLocation = $this->db->escape_string($location);
-        $intUserId = (int)$this->userId;
-
         $wordInfo = $this->findWord($initialWord);
         if (!$wordInfo)
             return;
-        $intInitialWordId = (int)$wordInfo["id"];
+        $initialWordId = $wordInfo["id"];
 
-        $this->db->query("INSERT INTO `Runs` (`UserID`, `InitialWordID`, `Time`, `LocationTMP`) " . 
-            "VALUES ('$intUserId', '$intInitialWordId', UNIX_TIMESTAMP(), '$escLocation')");
-        if (!$this->checkForDbError())
-            return;
-
-        $runId = $this->db->insert_id;
-
+        $checkedWords = array();
+        $wordsForInitialWord = array();
+        $wordsForLocation = array();
         foreach ($words as $wordData)
         {
-            $word = (string)@$wordData["word"];
-            $intTitlePos = (int)@$wordData["titlePos"];
-            $intLocationPos = (int)@$wordData["locationPos"];
-            $connotation = (string)@$wordData["connotation"];
-            $escConnotation = $this->db->escape_string($connotation);
+            $word = @$wordData["word"];
+            $titlePos = isset($wordData["titlePos"]) ? $wordData["titlePos"] : 0;
+            $locationPos = isset($wordData["locationPos"]) ? $wordData["locationPos"] : 0;
+            $connotation = @$wordData["connotation"];
+
+            if (!is_string($word) || $word == "." || $word == ".." ||
+                !is_int($titlePos) ||
+                !is_int($locationPos) ||
+                ($connotation !== "+" && $connotation !== "-"))
+                continue;
 
             $wordInfo = $this->findWord($word);
             if (!$wordInfo)
                 return;
-            $intWordId = (int)$wordInfo["id"];
 
-            $this->db->query("INSERT INTO `RunWords` " . 
-                "(`RunID`, `WordID`, `InitialWordNearness`, `LocationNearness`, `Connotation`) " . 
-                "VALUES ('$runId', '$intWordId', '$intTitlePos', '$intLocationPos', '$escConnotation')");
-            if (!$this->checkForDbError())
+            $checkedWords[] = array(
+                "id" => $wordInfo["id"],
+                "distanceFromInitialWord" => $titlePos,
+                "distanceFromLocation" => $locationPos,
+                "connotation" => $connotation,
+            );
+
+            if ($titlePos > 0)
+                $wordsForInitialWord[$titlePos - 1] = $wordInfo["id"];
+            if ($locationPos > 0)
+                $wordsForLocation[$locationPos - 1] = $wordInfo["id"];
+        }
+
+        $time = time();
+
+        if (!$this->addRunn($this->userId, $initialWordId, $time, $location))
+            return;
+
+        $runId = $this->db->insert_id;
+
+        foreach ($checkedWords as $wordInfo)
+        {
+            if (!$this->addRunWord($runId, $wordInfo["id"], $wordInfo["distanceFromInitialWord"],
+                $wordInfo["distanceFromLocation"], $wordInfo["connotation"]))
                 return;
         }
 
+        for ($i = 0; $i < count($wordsForInitialWord); $i++)
+        {
+            if (!isset($wordsForInitialWord[$i]))
+                continue;
+            $wordId = $wordsForInitialWord[$i];
+
+            $strength = 1 - $i * 0.02;
+            if ($strength > 0.000001)
+            {
+                if (!$this->addRelations($initialWordId, $wordId, $this->userId, $time, $strength))
+                    return;
+            }
+
+            for ($j = $i + 1; $j < count($wordsForInitialWord); $j++)
+            {
+                if (!isset($wordsForInitialWord[$j]))
+                    continue;
+                $otherWordId = $wordsForInitialWord[$j];
+
+                $strength = 1 - ($j - $i - 1) * 0.1;
+                if ($strength > 0.000001)
+                {
+                    if (!$this->addRelations($wordId, $otherWordId, $this->userId, $time, $strength))
+                        return;
+                }
+            }
+        }
+
+        for ($i = 0; $i < count($wordsForLocation); $i++)
+        {
+            if (!isset($wordsForLocation[$i]))
+                continue;
+            $wordId = $wordsForLocation[$i];
+
+            /*$strength = 1 - $i * 0.02;
+            if ($strength > 0.000001)
+            {
+                if (!$this->addRelations($initialWordId, $wordId, $this->userId, $time, $strength))
+                    return;
+            }*/
+
+            for ($j = $i + 1; $j < count($wordsForLocation); $j++)
+            {
+                if (!isset($wordsForLocation[$j]))
+                    continue;
+                $otherWordId = $wordsForLocation[$j];
+
+                $strength = 1 - ($j - $i - 1) * 0.1;
+                if ($strength > 0.000001)
+                {
+                    if (!$this->addRelations($wordId, $otherWordId, $this->userId, $time, $strength))
+                        return;
+                }
+            }
+        }
+
         $this->setResponseData(null);
+    }
+
+    protected function addRunn($userId, $initialWordId, $time, $location)
+    {
+        $intUserId = (int)$userId;
+        $intInitialWordId = (int)$initialWordId;
+        $intTime = (int)$time;
+        $escLocation = $this->db->escape_string((string)$location);
+
+        $this->db->query("INSERT INTO `Runs` (`UserID`, `InitialWordID`, `Time`, `LocationTMP`) " . 
+            "VALUES ('$intUserId', '$intInitialWordId', '$time', '$escLocation')");
+
+        return $this->checkForDbError();
+    }
+
+    protected function addRunWord($runId, $wordId, $distanceFromInitialWord, $distanceFromLocation, $connotation)
+    {
+        $intRunId = (int)$runId;
+        $intWordId = (int)$wordId;
+        $intDistanceFromInitialWord = (int)$distanceFromInitialWord;
+        $intDistanceFromLocation = (int)$distanceFromLocation;
+        $escConnotation = $this->db->escape_string((string)$connotation);
+
+        $this->db->query("INSERT INTO `RunWords` " . 
+            "(`RunID`, `WordID`, `DistanceFromInitialWord`, `DistanceFromLocation`, `Connotation`) VALUES " .
+            "('$intRunId', '$intWordId', '$intDistanceFromInitialWord', '$intDistanceFromLocation', '$escConnotation')");
+
+        return $this->checkForDbError();
+    }
+
+    protected function addRelations($word1Id, $word2Id, $userId, $time, $strength)
+    {
+        $intWord1Id = (int)$word1Id;
+        $intWord2Id = (int)$word2Id;
+        $intUserId = (int)$userId;
+        $intTime = (int)$time;
+        $floatStrength = (float)$strength;
+
+        $this->db->query("INSERT INTO `Relations` " . 
+            "(`FromWordID`, `ToWordID`, `UserID`, `Time`, `Strength`) VALUES " .
+            "('$intWord1Id', '$intWord2Id', '$intUserId', '$intTime', '$floatStrength') " .
+            "ON DUPLICATE KEY UPDATE `Strength` = `Strength` + '$floatStrength'");
+
+        $this->db->query("INSERT INTO `Relations` " . 
+            "(`FromWordID`, `ToWordID`, `UserID`, `Time`, `Strength`) VALUES " .
+            "('$intWord2Id', '$intWord1Id', '$intUserId', '$intTime', '$floatStrength') " .
+            "ON DUPLICATE KEY UPDATE `Strength` = `Strength` + '$floatStrength'");
+
+        return $this->checkForDbError();
     }
 
     protected function returnGraphData($word)

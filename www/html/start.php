08ca00085c2f580182a9ef95da528f086e3b76e9
maltefiala <maltefiala@gmx.at>
2011-03-04 20:17:42 +0100

    You want a piece of me? Checky gone sparky!

--- www/html/start.php
+++ www/html/start.php
@@ -5,17 +5,17 @@
         Willkommen bei gnovi.
           <br>
           <br>
-        Mit gnovi werden textuelle Assotiationen auf spielerische Weise kartographiert. Dazu startet man viele 
+        Mit gnovi werden textuelle Assoziationen auf spielerische Weise kartographiert. Dazu startet man viele 
           <a href="anlauf">Anläufe</a>. Bei jedem <a href="anlauf">Anlauf</a> wird eines von vierhundert handverlesenen Worten 
           zufällig ausgespuckt. Nun bleiben zwanzig Sekunden Zeit um die dazu assoziierten Worte zu Bildschirm zu bringen. Danach 
           kommt es zur Eingabe der momentanen Lokalität. Auch hier bleiben wiederum zwanzig Sekunden Zeit, Assoziationen 
-          aufzuschreiben. Im letzten Schritt geht es zur Konnotierung der eingegebenen Worte, die mit den Pfeiltasten oder der Maus 
+          aufzuschreiben. Im letzten Schritt geht es zur Konnotation der eingegebenen Worte, die mit den Pfeiltasten oder der Maus 
           erfolgt.
         <br>
         <br>
         Nach jedem <a href="anlauf">Anlauf</a> wird die Assoziationskarte angezeigt. Ein Klick auf einen Knotenpunkt zentriert diesen.  
-          Wurde das ausgewählte Wort schon einmal eingegeben, so erscheint eine Zeitleiste, mit welcher Assoziationen zum jeweiligen 
-          Zeitpunkt angezeigt werden. Alternativ dazu werden Assoziationskarten über die Links <a href="ich">ich</a> und <a 
+          Wurde das ausgewählte Wort bereits eingegeben, so erscheint eine Zeitleiste, mit welcher Assoziationen zum jeweiligen 
+          Zeitpunkt eingeblendet werden können. Alternativ dazu werden Assoziationskarten über die Links <a href="ich">ich</a> und <a 
           href="wir">wir</a> erreicht. <a href="ich">Ich</a> dient dazu, allein die eigenen Assoziationen zu durchwandern. Unter <a 
           href="wir">wir</a> hingegen werden die Assoziationen aller Teilnehmer_innen angezeigt.
         <br>
--- www/js/html5check.js
+++ www/js/html5check.js
@@ -1,6 +1,45 @@
 /* Thanks to Modernizr.com for their great effort */
 
-if (Modernizr.canvas && Modernizr.canvastext && Modernizr.rgba && Modernizr.hsla && Modernizr.borderradius && Modernizr.opacity && Modernizr.csscolumns) {
-} else {
+
+var errormsg = "";
+var error = false;
+
+if (!Modernizr.canvas) {
+    errormsg = errormsg + "canvas";    
+    error = true;
+}
+
+if (!Modernizr.canvastext) {
+    errormsg = errormsg + "canvastext";
+    error = true;
+    }
+
+if (!Modernizr.rgba) {
+    errormsg = errormsg + "rgba";
+    error = true;
+}
+
+if (!Modernizr.hsla) {
+    errormsg = errormsg + "hsla";
+    error = true;
+}
+
+if (!Modernizr.borderradius) {
+    errormsg = errormsg + "borderradius";
+    error = true;
+}
+
+if (!Modernizr.opacity) {
+    errormsg = errormsg + "opacity";
+    error = true;
+}
+
+if (!Modernizr.csscolumns) {
+    errormsg = errormsg + "csscolumns";
+}
+
+if (error) {
     window.location = "html5";
 }
+
+/* throw(errormsg); */

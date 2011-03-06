3a9b844509b1d2aa893ddea4818be9eb9e65a402
maltefiala <maltefiala@gmx.at>
2011-03-06 23:46:23 +0100

    links and graph

--- www/html/links.php
+++ www/html/links.php
@@ -1,15 +1,89 @@
     <section id="multicolumn">
-      Es war ein Herbstnach-mittag, als eine alter Freund und ich auf die Idee kamen, eine Weltkarte der anderen Art zu erschaffen. In 
-        unserem kindlichen übermut wollten wir die gesamte Welt kartographieren, jeden Zusammenhang in der Geschichte der Menschheit 
-        aufzeigen. Mit der Zeit kam Erfahrung und damit die Einsicht, dass ein solches Unterfangen ein Ding der Unmöglichkeit 
-        ist.
+      Ohne Gedanken keine Idee:
+      <ul>
+	    <li>
+	      <a title="http://spectrumatlas.org/" rel="nofollow" href="http://spectrumatlas.org/">Atlas Of Electromagnetic Space
+	      </a>
+	    </li>
+	    <li>
+	      <a title="http://www.jibble.org/piespy/" rel="nofollow" href="http://www.jibble.org/piespy/">Piespy Social Network Bot
+	      </a>
+        </li>
+	    <li>
+	      <a title="http://beat.doebe.li/bibliothek/w00434.html" rel="nofollow" href="http://beat.doebe.li/bibliothek/w00434.html">Karte 
+	        der Verknüpfungen der Freudschen Lehre des Es-Ich und Über-Ich
+	      </a>
+        </li>
+	    <li>
+	      <a title="http://de.wikipedia.org/wiki/Stufenmodell_der_psychosozialen_Entwicklung#Stufe_8:_Ich-
+	      Integrit.C3.A4t_vs._Verzweiflung_.28Hohes_Erwachsenenalter.2FReife.29" rel="nofollow" href="http://de.wikipedia.org/wiki/
+	      Stufenmodell_der_psychosozialen_Entwicklung#Stufe_8:_Ich-Integrit.C3.A4t_vs._Verzweiflung_.28Hohes_Erwachsenenalter.2FReife.
+	      29">Das Stufenmodell der psychosozialen Entwicklung (von Erik H. Erikson)
+	      </a>
+        </li>
+	    <li>
+	      <a title="http://www.psychwiki.com/wiki/Computational_Modeling" rel="nofollow" href="http://www.psychwiki.com/wiki/
+	        Computational_Modeling">Computational Modeling
+	      </a>
+        </li>
+	    <li>
+	      <a title="http://www.cg.tuwien.ac.at/courses/InfoVis/" rel="nofollow" href="http://www.cg.tuwien.ac.at/courses/InfoVis/">Die 
+	        Infovis Vorlesung an der TU
+	      </a>
+        </li>
+	    <li>
+	      <a title="http://en.wikipedia.org/wiki/Graph_theory" rel="nofollow" href="http://en.wikipedia.org/wiki/Graph_theory">Diverse 
+	        Graphentheorien
+	      </a>
+        </li>
+	    <li>
+	      <a title="http://www.chrisharrison.net" rel="nofollow" href="http://www.chrisharrison.net/">Gestalterisch anspruchsvolle 
+	        Informationsvisualisierungen
+	      </a>
+        </li>
+	    <li>
+	      <a title="http://www.javacoffeebreak.com/tutorials/aisearch/chapter6.html" rel="nofollow" href="http://www.javacoffeebreak.com/
+	        tutorials/aisearch/chapter6.html">Informationstechnologische Konzepte wie die Breitensuche
+          </a>
+        </li>
+	    <li>
+	      <a title="http://www.heise.de/tr/artikel/Das-automatisierte-soziale-Netzwerk-1001883.html" rel="nofollow" href="http://
+	        www.heise.de/tr/artikel/Das-automatisierte-soziale-Netzwerk-1001883.html">Die Idee automatisierter sozialer Netzerke
+	      </a>
+        </li>
+	    <li>
+	      <a title="http://www.cfinder.org/" rel="nofollow" href="http://www.cfinder.org/">Das Clustering von Nodes
+	      </a>
+        </li>
+	    <li>
+	      <a title="http://en.wikipedia.org/wiki/Graphviz" rel="nofollow" href="http://en.wikipedia.org/wiki/Graphviz">Open Graphviz
+	      </a>
+        </li>
+	    <li>
+	      <a title="http://www.wikiviz.org/wiki/Main_Page" rel="nofollow" href="http://www.wikiviz.org/wiki/Main_Page">Wikis zur 
+	        Veranschaulichung von Visualiserungen
+	      </a>
+        </li>
+	    <li>
+	      <a title="http://networkx.lanl.gov/" rel="nofollow" href="http://networkx.lanl.gov/">Tools aus der Software- und Netzerkanalyse
+	      </a>
+        </li>
+	    <li>
+	      <a title="http://www.visualcomplexity.com/" rel="nofollow" href="http://www.visualcomplexity.com/">Visual Complexity zum 
+	        Aufzeigen der eigenen gestalterischen Möglichkeiten
+	      </a>
+        </li>
+      </ul>
       <br>
-      <br> 
-      Fürs erste müsse es wohl genügen, sich auf das Selbst zu konzentrieren um im nächsten Schritt auf 
-        die Erfahrungen anderer zu schlie&szlig;en. Zumindest kleine Karten kann man zeichnen. War die Karte aller externen Verbindungen 
-        unvorstellbar, so hatte die Idee einer Karte der internen Welt doch seinen Reiz. 
-      <br>
-      <br>
-      Die Funktionsweise gnovis im Detail wird über die nächsten Wochen im <a href="http://blog.gnovi.org">Weblog</a> und an 
-        dieser Stelle erörtert.<br><br> Bedanken möchte ich mich herzlichst bei Friedrich Schöller, der mir in der Programmierung mehr als nur unter die Arme gegriffen hat. Außerdem natürlich bei der Magistratabteilung 
+      Ohne Handwerk kein Projekt:
+      <ul>
+  	    <li>
+	      <a title="http://wortschatz.uni-leipzig.de/axis/servlet/ServiceOverviewServlet" rel="nofollow" href="http://wortschatz.uni-
+	        leipzig.de/axis/servlet/ServiceOverviewServlet">Die Wortschatz-Datenbank der Universität Leipzig
+	      </a>
+        </li>
+	    <li>
+	      <a title="MooTools" href="http://mootools.net/">Mootools</a>
+	    </li>
+      </ul>
     </section>
